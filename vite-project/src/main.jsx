9e377d4440508fd9d89654678260b0a6e211bc43
Ida Whitcomb <idawhitcomb@gmail.com>
2024-02-28 10:16:22 -0500

    Continued setting up file structure and adding information to Error.jsx, main.jsx, and NavTabs.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import App from './App.jsx'
+import Error from './pages/Error.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Resume from './pages/Resume.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
