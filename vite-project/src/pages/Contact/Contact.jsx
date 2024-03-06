ab222c644314d0153e2739b4cdc461cd20cf84fe
Ida Whitcomb <idawhitcomb@gmail.com>
2024-03-06 16:05:23 -0500

    Added modal pop-ups for contact page

--- vite-project/src/pages/Contact/Contact.jsx
+++ vite-project/src/pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { validateName, validateEmail } from '../../components/FormValidation';
+import styles from '../Contact/Contact.css';
 
 export default function Contact() {
   const [name, setName] = useState('');
@@ -7,6 +8,8 @@ export default function Contact() {
   const [message, setMessage] = useState('');
   const [nameError, setNameError] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const handleNameChange = (event) => {
     const nameValue = event.target.value;
@@ -20,23 +23,57 @@ export default function Contact() {
     setEmailError(validateEmail(emailValue));
   };
 
+  const handleBlur = (event) => {
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+    let errorMessage = '';
+    if(fieldName === 'name') {
+      errorMessage = validateName(fieldValue);
+      setNameError(errorMessage);
+    } else if (fieldName === 'email') {
+      errorMessage = validateEmail(fieldValue);
+      setEmailError(errorMessage);
+    }
+
+    if(errorMessage) {
+      setModalMessage(errorMessage);
+      setShowModal(true);
+    } else {
+      setShowModal(false);
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
     return (
     <div>
         <h1>Contact Me</h1>
       <div className="input-group mb-3">
         <span className="input-group-text" id="basic-addon1">Name</span>
-        <input type="text" className="form-control" placeholder="Name" aria-label="Name" value={name} onChange={handleNameChange} onBlur={handleNameChange}></input>
-        {nameError && <div className='error'>{nameError}</div>}
+        <input type="text" className="form-control" placeholder="Name" aria-label="Name" value={name} onChange={handleNameChange} onBlur={handleBlur} name="name"></input>
       </div>
       <div className="input-group mb-3">
         <span className="input-group-text" id="basic-addon1">Email Address</span>
-        <input type="text" className="form-control" placeholder="Email Address" aria-label="Email Address" value={email} onChange={handleEmailChange} onBlur={handleEmailChange}></input>
-        {emailError && <div className="error">{emailError}</div>}
+        <input type="text" className="form-control" placeholder="Email Address" aria-label="Email Address" value={email} onChange={handleEmailChange} onBlur={handleBlur} name="email"></input>
       </div>
       <div className="input-group">
         <span className="input-group-text">Message</span>
         <textarea className="form-control" aria-label="Message" value={message} onChange={(event) => setMessage (event.target.value)}></textarea>
       </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+              <p>{modalMessage}</p>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeModal}></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
     );
   }
