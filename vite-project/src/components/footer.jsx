19242c71b925d0f638e431f8a6a1a1ca36005a58
Ida Whitcomb <idawhitcomb@gmail.com>
2024-03-07 11:14:42 -0500

    Added downloadable resume to my resume page

--- vite-project/src/components/footer.jsx
+++ vite-project/src/components/footer.jsx
@@ -1,9 +1,12 @@
 export default function Footer() {
     return (
-      <div>
-        <h1></h1>
-        <p>
-        </p>
+      <div className="container">
+      <div className="row">
+        <div className="col">
+          <a href="https://github.com/Ida-Whit" className="btn btn-primary">GitHub</a>
+          <a href="#" className="btn btn-primary">LinkedIn</a>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- vite-project/src/pages/Resume/Resume.jsx
+++ vite-project/src/pages/Resume/Resume.jsx
@@ -1,9 +1,11 @@
 export default function Resume() {
-    return (
+  const resumePath = './utils/files/resume/pdf';
+
+  return (
       <div>
-        <h1>Resume</h1>
-        <p>
-        </p>
+          <h1>Resume</h1>
+          <p>Click the link below to download my resume:</p>
+          <a href={resumePath} download="resume.pdf">Download Resume</a>
       </div>
     );
   }
